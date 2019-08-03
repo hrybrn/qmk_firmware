6df07b7c53de767242eb21925b38fb2d6ab1bd70
hrybrn <harryjgbrown@gmail.com>
2019-08-03 13:04:12 +0100

    general refactor

--- keyboards/dz60/keymaps/60_tsangan_mine/keymap.c
+++ keyboards/dz60/keymaps/60_tsangan_mine/keymap.c
@@ -1,7 +1,7 @@
 #include QMK_KEYBOARD_H
 
+// macro keycodes
 enum custom_keycodes {
-  // macros
   BACK_TAB = SAFE_RANGE,
   FORWARD_TAB,
   CLOSE_TAB_WIN,
@@ -10,7 +10,7 @@ enum custom_keycodes {
   SEARCH_WIN
 };
 
-// macros
+// macro functions
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case BACK_TAB:
@@ -29,67 +29,48 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   return true;
 }
 
-// tap dance
-typedef struct {
-  bool is_press_action;
-  int state;
-} tap;
-
+// generic tap dance setup
 enum {
-  SINGLE_TAP = 1,
-  SINGLE_HOLD = 2,
-  DOUBLE_TAP = 3,
-  DOUBLE_HOLD = 4,
-  DOUBLE_SINGLE_TAP = 5, //send two single taps
-  TRIPLE_TAP = 6,
-  TRIPLE_HOLD = 7
-};
-
-//Tap dance enums
-enum {
-  CAPS_SPECIAL = 0,
+  SINGLE_TAP = 0,
+  SINGLE_HOLD,
+  DOUBLE_TAP,
+  DOUBLE_HOLD,
+  TRIPLE_TAP,
+  TRIPLE_HOLD
 };
 
 int cur_dance (qk_tap_dance_state_t *state) {
   if (state->count == 1) {
     if (state->interrupted || !state->pressed)  return SINGLE_TAP;
-    //key has not been interrupted, but they key is still held. Means you want to send a 'HOLD'.
     else return SINGLE_HOLD;
   }
   else if (state->count == 2) {
-    /*
-     * DOUBLE_SINGLE_TAP is to distinguish between typing "pepper", and actually wanting a double tap
-     * action when hitting 'pp'. Suggested use case for this return value is when you want to send two
-     * keystrokes of the key, and not the 'double tap' action/macro.
-    */
-    if (state->interrupted) return DOUBLE_SINGLE_TAP;
-    else if (state->pressed) return DOUBLE_HOLD;
+    if (state->pressed) return DOUBLE_HOLD;
     else return DOUBLE_TAP;
   }
-  //Assumes no one is trying to type the same letter three times (at least not quickly).
-  //If your tap dance key is 'KC_W', and you want to type "www." quickly - then you will need to add
-  //an exception here to return a 'TRIPLE_SINGLE_TAP', and define that enum just like 'DOUBLE_SINGLE_TAP'
   if (state->count == 3) {
     if (state->interrupted || !state->pressed)  return TRIPLE_TAP;
     else return TRIPLE_HOLD;
   }
-  else return 8; //magic number. At some point this method will expand to work for more presses
+  else return 8;
 }
 
-static tap xtap_state = {
-  .is_press_action = true,
-  .state = 0
+// tap dance keycodes
+enum {
+  CAPS_SPECIAL = 0
 };
 
+// tap dance functions
 void tap_dance_caps (qk_tap_dance_state_t *state, void *user_data) {
-  xtap_state.state = cur_dance(state);
-  switch (xtap_state.state) {
+  int tap_result = cur_dance(state);
+  switch (tap_result) {
     case SINGLE_TAP: SEND_STRING(SS_TAP(X_F19)); break;
     case DOUBLE_TAP: SEND_STRING("();" SS_TAP(X_ENTER)); break;
     case TRIPLE_TAP: reset_keyboard(); break;
   }
 }
 
+// tap dance setup
 qk_tap_dance_action_t tap_dance_actions[] = {
   [CAPS_SPECIAL] = ACTION_TAP_DANCE_FN(tap_dance_caps)
 };
