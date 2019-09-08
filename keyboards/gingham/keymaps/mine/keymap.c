76296bd84758d495b6ff9d7bdd5fabc130ddb9c7
hrybrn <harryjgbrown@gmail.com>
2019-09-08 18:37:05 +0100

    first attempt at layout for gingham

---
+++ keyboards/gingham/keymaps/mine/keymap.c
@@ -0,0 +1,173 @@
+#include QMK_KEYBOARD_H
+#include "i2c_master.h"
+
+// macro keycodes
+enum custom_keycodes {
+  BACK_TAB = SAFE_RANGE,
+  FORWARD_TAB,
+  CLOSE_TAB,
+  SEARCH,
+  LGUI_SWITCH,
+  LALT_SWITCH,
+  WINDOWS_MODE_TOGGLE,
+  COPYPASTA_HELI,
+  COPYPASTA_CHEAT,
+  COPYPASTA_GREEK_SALT,
+  COPYPASTA_DARTH_PLAGUEIS,
+  WINDOWS_KEY_TOGGLE
+};
+
+bool windows_mode = false;
+
+bool windows_key_enabled = true;
+
+// macro functions
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case BACK_TAB:
+      if (record->event.pressed) SEND_STRING(SS_LCTRL(SS_LSFT(SS_TAP(X_TAB)))); break;
+    case FORWARD_TAB:
+      if (record->event.pressed) SEND_STRING(SS_LCTRL(SS_TAP(X_TAB))); break;
+    case WINDOWS_MODE_TOGGLE:
+      if (record->event.pressed) {
+        windows_mode = !windows_mode;
+
+        uint8_t send_data;
+
+        if (windows_mode) {
+          send_data = 0x18;
+        } else {
+          send_data = 0x10;
+        }
+
+        i2c_writeReg((PORT_EXPANDER_ADDRESS << 1), 0x09, &send_data, 1, 20);
+      }
+      break;
+    case WINDOWS_KEY_TOGGLE:
+      if (record->event.pressed) {
+        windows_key_enabled = !windows_key_enabled;
+      }
+      break;
+    case COPYPASTA_HELI:
+      if (record->event.pressed) SEND_STRING("I sexually Identify as an Attack Helicopter. Ever since I was a boy I dreamed of soaring over the oilfields dropping hot sticky loads on disgusting foreigners. People say to me that a person being a helicopter is Impossible and I’m fucking retarded but I don’t care, I’m beautiful. I’m having a plastic surgeon install rotary blades, 30 mm cannons and AMG-114 Hellfire missiles on my body. From now on I want you guys to call me “Apache” and respect my right to kill from above and kill needlessly. If you can’t accept me you’re a heliphobe and need to check your vehicle privilege. Thank you for being so understanding."); break;
+    case COPYPASTA_CHEAT:
+      if (record->event.pressed) SEND_STRING("You cheated not only the game, but yourself. You didn't grow. You didn't improve. You took a shortcut and gained nothing. You experienced a hollow victory. Nothing was risked and nothing was gained. It's sad that you don't know the difference."); break;
+    case COPYPASTA_GREEK_SALT:
+      if (record->event.pressed) SEND_STRING("High in orbit, the Gitraktmaet motherships descend upon the Earth. They prepare to enslave the world and mine it for all its salt, but the scanners detect an abnormally high concentration inside a tiny shack in Greece. The invasion won't be necessary. \"Lock onto him with the RNG disruptor,\" says the captain, greedily. \"Soon we shall have all the salt we need.\""); break;
+    case COPYPASTA_DARTH_PLAGUEIS:
+      if (record->event.pressed) SEND_STRING("Did you ever hear the tragedy of Darth Plagueis The Wise? I thought not. It’s not a story the Jedi would tell you. It’s a Sith legend. Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midichlorians to create life… He had such a knowledge of the dark side that he could even keep the ones he cared about from dying. The dark side of the Force is a pathway to many abilities some consider to be unnatural. He became so powerful… the only thing he was afraid of was losing his power, which eventually, of course, he did. Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep. Ironic. He could save others from death, but not himself."); break;
+  }
+
+  if (windows_mode) {
+    switch (keycode) {
+      case CLOSE_TAB:
+        if (record->event.pressed) SEND_STRING(SS_LCTRL("w")); break;
+      case SEARCH:
+        if (record->event.pressed) SEND_STRING(SS_LCTRL("ctv") SS_TAP(X_ENTER)); break;
+      case LGUI_SWITCH:
+        if (record->event.pressed) register_code(KC_LALT);
+        else unregister_code(KC_LALT);
+        break;
+      case LALT_SWITCH:
+        if (windows_key_enabled) {
+          if (record->event.pressed) register_code(KC_LGUI);
+          else unregister_code(KC_LGUI);
+        }
+        break;
+    }
+  } else {
+    switch (keycode) {
+      case CLOSE_TAB:
+        if (record->event.pressed) SEND_STRING(SS_LGUI("w")); break;
+      case SEARCH:
+        if (record->event.pressed) SEND_STRING(SS_LGUI("ctv") SS_TAP(X_ENTER)); break;
+      case LALT_SWITCH:
+        if (record->event.pressed) register_code(KC_LALT);
+        else unregister_code(KC_LALT);
+        break;
+      case LGUI_SWITCH:
+        if (windows_key_enabled) {
+          if (record->event.pressed) register_code(KC_LGUI);
+          else unregister_code(KC_LGUI);
+        }
+        break;
+    }
+  }
+  return true;
+}
+
+// generic tap dance setup
+enum {
+  SINGLE_TAP = 0,
+  SINGLE_HOLD,
+  DOUBLE_TAP,
+  DOUBLE_HOLD,
+  TRIPLE_TAP,
+  TRIPLE_HOLD
+};
+
+int cur_dance (qk_tap_dance_state_t *state) {
+  if (state->count == 1) {
+    if (state->interrupted || !state->pressed)  return SINGLE_TAP;
+    else return SINGLE_HOLD;
+  }
+  else if (state->count == 2) {
+    if (state->pressed) return DOUBLE_HOLD;
+    else return DOUBLE_TAP;
+  }
+  if (state->count == 3) {
+    if (state->interrupted || !state->pressed)  return TRIPLE_TAP;
+    else return TRIPLE_HOLD;
+  }
+  else return 8;
+}
+
+// tap dance keycodes
+enum {
+  CAPS_SPECIAL = 0,
+  FN_SPECIAL
+};
+
+// tap dance functions
+void tap_dance_caps (qk_tap_dance_state_t *state, void *user_data) {
+  switch (cur_dance(state)) {
+    case SINGLE_TAP: SEND_STRING(SS_TAP(X_F19)); break;
+    case DOUBLE_TAP: SEND_STRING("();" SS_TAP(X_ENTER)); break;
+    case TRIPLE_TAP: reset_keyboard(); break;
+  }
+}
+
+// tap dance setup
+qk_tap_dance_action_t tap_dance_actions[] = {
+  [CAPS_SPECIAL] = ACTION_TAP_DANCE_FN(tap_dance_caps)
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+[0] = LAYOUT_60_ansi_split_bs_rshift( /* Base */
+    KC_ESC,  KC_1,     KC_2,     KC_3,  KC_4,  KC_5,  KC_6,    KC_7,  KC_8,  KC_9,     KC_0,     KC_MINS,  KC_EQL,  KC_BSPC, KC_NO,
+    KC_TAB,   KC_Q,     KC_W,     KC_E,  KC_R,  KC_T,  KC_Y,    KC_U,  KC_I,  KC_O,     KC_P,     KC_LBRC,  KC_RBRC, KC_BSLS,
+    TD(CAPS_SPECIAL),  KC_A,     KC_S,     KC_D,  KC_F,  KC_G,  KC_H,    KC_J,  KC_K,  KC_L,     KC_SCLN,  KC_QUOT,           KC_ENT, 
+    KC_LSFT,            KC_Z,     KC_X,  KC_C,  KC_V,  KC_B,    KC_N,  KC_M,  KC_COMM,  KC_DOT,   KC_SLSH,  RSFT_T(KC_UP), RSFT_T(KC_UP),
+    KC_LCTL,  LALT_SWITCH, LGUI_SWITCH,                         KC_SPC,                          KC_LGUI,  LT(1, KC_LEFT),  RALT_T(KC_DOWN), RCTL_T(KC_RGHT)),
+
+[1] = LAYOUT_60_ansi_split_bs_rshift( /* FN */
+    KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_DEL, KC_TRNS,
+    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_UP,  RESET,    KC_TRNS,
+    WINDOWS_KEY_TOGGLE, KC_VOLD,  KC_VOLU,  KC_MUTE,  KC_MPLY,  KC_NO,    KC_NO,    KC_NO,    KC_PGUP,  KC_HOME,  KC_LEFT,  KC_RGHT,            KC_NO,
+    WINDOWS_MODE_TOGGLE, BACK_TAB, FORWARD_TAB, CLOSE_TAB, SEARCH,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_PGDN, KC_END, KC_DOWN, LT(2,KC_NO),  LT(2,KC_NO),
+    KC_VOLU,  KC_VOLD,  KC_MUTE,                                KC_TRNS,                                KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS),
+
+[2] = LAYOUT_60_ansi_split_bs_rshift( /* Empty for dynamic keymaps */
+    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS,
+    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,
+    KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+    KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                                KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS),
+
+[3] = LAYOUT_60_ansi_split_bs_rshift( /* Empty for dynamic keymaps */
+    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS,
+    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,
+    KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+    KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                                KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS),
+};
