37151174cc2c68a932f5da76cfaa0ec553ba6436
hbrown3 <harry_brown@intuit.com>
2019-09-13 17:09:15 +0100

    added more macros for qbo

--- keyboards/dz60/keymaps/60_tsangan_mine/keymap.c
+++ keyboards/dz60/keymaps/60_tsangan_mine/keymap.c
@@ -12,7 +12,10 @@ enum custom_keycodes {
   COPYPASTA_HELI,
   COPYPASTA_CHEAT,
   COPYPASTA_GREEK_SALT,
-  COPYPASTA_DARTH_PLAGUEIS
+  COPYPASTA_DARTH_PLAGUEIS,
+  DEFAULT_QBO_PASSWORD,
+  OPEN_PAGE_IN_NEW_INCOGNITO,
+  US_PHONE_NUMBER
 };
 
 bool windows_mode = false;
@@ -37,6 +40,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       if (record->event.pressed) SEND_STRING("High in orbit, the Gitraktmaet motherships descend upon the Earth. They prepare to enslave the world and mine it for all its salt, but the scanners detect an abnormally high concentration inside a tiny shack in Greece. The invasion won't be necessary. \"Lock onto him with the RNG disruptor,\" says the captain, greedily. \"Soon we shall have all the salt we need.\""); break;
     case COPYPASTA_DARTH_PLAGUEIS:
       if (record->event.pressed) SEND_STRING("Did you ever hear the tragedy of Darth Plagueis The Wise? I thought not. It’s not a story the Jedi would tell you. It’s a Sith legend. Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midichlorians to create life… He had such a knowledge of the dark side that he could even keep the ones he cared about from dying. The dark side of the Force is a pathway to many abilities some consider to be unnatural. He became so powerful… the only thing he was afraid of was losing his power, which eventually, of course, he did. Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep. Ironic. He could save others from death, but not himself."); break;
+    case DEFAULT_QBO_PASSWORD:
+      if (record->event.pressed) SEND_STRING("Password1!"); break;
+    case US_PHONE_NUMBER:
+      if (record->event.pressed) SEND_STRING("5555555555");
   }
 
   if (windows_mode) {
@@ -53,6 +60,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         if (record->event.pressed) register_code(KC_LGUI);
         else unregister_code(KC_LGUI);
         break;
+      case OPEN_PAGE_IN_NEW_INCOGNITO:
+        if (record->event.pressed) SEND_STRING(SS_LCTRL("lcw" SS_LSFT("n") "v") SS_TAP(X_ENTER)); break;
     }
   } else {
     switch (keycode) {
@@ -67,6 +76,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       case LGUI_SWITCH:
         if (record->event.pressed) register_code(KC_LGUI);
         else unregister_code(KC_LGUI);
+        break;
+      case OPEN_PAGE_IN_NEW_INCOGNITO:
+        if (record->event.pressed) {
+          SEND_STRING(SS_LGUI("lcw"));
+          _delay_ms(25);
+          SEND_STRING(SS_LGUI(SS_LSFT("n") "v") SS_TAP(X_ENTER));
+        }
         break;
     }
   }
@@ -121,8 +137,8 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 // keymap
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT_60_tsangan(KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, TD(CAPS_SPECIAL), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_UP), LT(1,KC_NO), KC_LCTRL, LALT_SWITCH, LGUI_SWITCH, KC_SPC, RGUI_T(KC_LEFT), RALT_T(KC_DOWN), RCTL_T(KC_RGHT)),
-	[1] = LAYOUT_60_tsangan(KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL, RGB_TOG, RGB_RMOD, RGB_MOD, RGB_HUD, RGB_HUI, RGB_SAD, RGB_SAI, RGB_VAD, RGB_VAI, RGB_SPD, RGB_SPI, KC_UP, KC_NO, KC_NO, TO(3), KC_VOLD, KC_VOLU, KC_MUTE, KC_MPLY, KC_NO, KC_NO, KC_NO, KC_PGUP, KC_HOME, KC_LEFT, KC_RGHT, KC_NO, WINDOWS_MODE_TOGGLE, BACK_TAB, FORWARD_TAB, CLOSE_TAB, SEARCH, KC_NO, KC_NO, KC_NO, KC_PGDN, KC_END, KC_DOWN, LT(2,KC_NO), KC_NO, MAGIC_UNSWAP_ALT_GUI, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO),
-	[2] = LAYOUT_60_tsangan(KC_NO, COPYPASTA_HELI, COPYPASTA_CHEAT, COPYPASTA_GREEK_SALT, COPYPASTA_DARTH_PLAGUEIS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_MS_U, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_MS_L, KC_MS_D, KC_MS_R, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_BTN1, KC_NO, KC_NO, KC_NO),
+	[1] = LAYOUT_60_tsangan(KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL, RGB_TOG, RGB_RMOD, RGB_MOD, RGB_HUD, RGB_HUI, RGB_SAD, RGB_SAI, RGB_VAD, RGB_VAI, RGB_SPD, RGB_SPI, KC_UP, KC_NO, KC_NO, TO(3), KC_VOLD, KC_VOLU, KC_MUTE, KC_MPLY, KC_NO, KC_NO, KC_NO, KC_PGUP, KC_HOME, KC_LEFT, KC_RGHT, KC_NO, WINDOWS_MODE_TOGGLE, BACK_TAB, FORWARD_TAB, CLOSE_TAB, SEARCH, OPEN_PAGE_IN_NEW_INCOGNITO, KC_NO, KC_NO, KC_PGDN, KC_END, KC_DOWN, LT(2,KC_NO), KC_NO, MAGIC_UNSWAP_ALT_GUI, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO),
+	[2] = LAYOUT_60_tsangan(KC_NO, COPYPASTA_HELI, COPYPASTA_CHEAT, COPYPASTA_GREEK_SALT, COPYPASTA_DARTH_PLAGUEIS, DEFAULT_QBO_PASSWORD, US_PHONE_NUMBER, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_MS_U, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_MS_L, KC_MS_D, KC_MS_R, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_BTN1, KC_NO, KC_NO, KC_NO),
 	[3] = LAYOUT_60_tsangan(KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, KC_NO, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, LT(4,KC_NO), KC_LCTRL, KC_NO, KC_LALT, KC_SPC, KC_NO, KC_RALT, KC_RCTL),
 	[4] = LAYOUT_60_tsangan(KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL, RGB_TOG, RGB_RMOD, RGB_MOD, RGB_HUD, RGB_HUI, RGB_SAD, RGB_SAI, RGB_VAD, RGB_VAI, RGB_SPD, RGB_SPI, KC_UP, KC_NO, KC_NO, TO(0), KC_VOLD, KC_VOLU, KC_MUTE, KC_MPLY, KC_NO, KC_NO, KC_NO, KC_PGUP, KC_HOME, KC_LEFT, KC_RGHT, KC_NO, KC_NO, KC_NO ,KC_NO ,KC_NO ,KC_NO, KC_NO, KC_NO, KC_NO, KC_PGDN, KC_END, KC_DOWN, LT(2,KC_NO), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO)
 };
